2332a264e60104ef9f1e5cd0a039408ca5be237d
atomicparade <47916875+atomicparade@users.noreply.github.com>
2023-03-04 13:41:07 -0500

    Implement support for custom reference regex

--- pelican/plugins/footnote_linker/footnote_linker.py
+++ pelican/plugins/footnote_linker/footnote_linker.py
@@ -3,29 +3,78 @@
 import logging
 import re
 
-from typing import Union
+from dataclasses import dataclass
+from typing import Pattern, Union
 
-from pelican import ArticlesGenerator, PagesGenerator, signals  # type: ignore
+from pelican import ArticlesGenerator, PagesGenerator, Pelican, signals  # type: ignore
 from pelican.contents import Article, Page  # type: ignore
 
 logger = logging.getLogger(__name__)
 
-RE_REF = re.compile("((?:.|\\n)*?)\\[ref(\\d+)\\]")
+CONTENT_REGEX = r"((?:.|\n)*?)"
+
+REFERENCE_REGEX_DEFAULT = r"\[ref\d+\]"
+RE_REFERENCE_DEFAULT: Pattern[str]
+
 REF_SUBNAMES = "abcdefghijklmnopqrstuvwxyz"
 
-RE_FOOTNOTE_HEADING = re.compile("<h[234]>Footnotes")
+RE_FOOTNOTE_HEADING = re.compile(r"<h[234]>Footnotes")
 
-RE_FOOTNOTE = re.compile("((?:.|\\n)*?)<p>\\[ref(\\d+)\\]((?:.|\\n)*?)</p>")
+RE_FOOTNOTE_DEFAULT: Pattern[str]
+
+
+def make_footnote_regex(regex: str) -> str:
+    return f"{CONTENT_REGEX}<p>({regex}){CONTENT_REGEX}</p>"
+
+
+# pylint: disable=global-statement
+def initalize_plugin(pelican_: Pelican) -> None:
+    global RE_REFERENCE_DEFAULT
+    global RE_FOOTNOTE_DEFAULT
+
+    pattern = pelican_.settings.get("REFERENCE_REGEX", REFERENCE_REGEX_DEFAULT)
+    RE_REFERENCE_DEFAULT = re.compile(f"{CONTENT_REGEX}({pattern})")
+    RE_FOOTNOTE_DEFAULT = re.compile(make_footnote_regex(pattern))
+
+
+@dataclass
+class Footnote:
+    num: int
+    html_id: str
+    ref_count: int
+
+    def __init__(self, num: int):
+        self.num = num
+        self.html_id = f"note-{self.num}"
+        self.ref_count = 0
+
+    def add_ref(self) -> str:
+        self.ref_count += 1
+        return self.get_ref_html_id(self.ref_count)
+
+    def get_ref_name(self, ref_num: int) -> str:
+        return REF_SUBNAMES[ref_num - 1]
+
+    def get_ref_html_id(self, ref_num: int) -> str:
+        return f"ref-{self.num}{self.get_ref_name(ref_num)}"
+
 
 # pylint: disable=protected-access,too-many-locals,too-many-statements
 def link_footnotes(item: Union[Article, Page]) -> None:
-    """Process the references on an article or page."""
+    """Link the references and footnotes on an article or page to each other."""
+    reference_regex = item.metadata.get("referenceregex", None)
+
+    if reference_regex:
+        re_reference = re.compile(f"{CONTENT_REGEX}{reference_regex}")
+        re_footnote = re.compile(make_footnote_regex(reference_regex))
+    else:
+        re_reference = RE_REFERENCE_DEFAULT
+        re_footnote = RE_FOOTNOTE_DEFAULT
 
     # Process references
+    ref_matches = list(re_reference.finditer(item._content))
 
-    ref_matches = list(RE_REF.finditer(item._content))
-
-    footnote_references: dict[str, list[str]] = {}
+    footnotes: dict[str, Footnote] = {}
 
     if len(ref_matches) == 0:
         # Document has no references
@@ -41,7 +90,7 @@ def link_footnotes(item: Union[Article, Page]) -> None:
         )
         return
 
-    footnote_matches = list(RE_FOOTNOTE.finditer(item._content))
+    footnote_matches = list(re_footnote.finditer(item._content))
 
     if len(footnote_matches) == 0:
         logger.warning(
@@ -59,10 +108,12 @@ def link_footnotes(item: Union[Article, Page]) -> None:
 
     for match in ref_matches:
         content_before = match.group(1)
-        note_num = match.group(2)
+        footnote_name = match.group(2)
 
         ref_pos = match.span(2)[0]
 
+        # If there's a space immediately before the reference, replace it with
+        # a non-breaking space to keep the last word and the reference together
         if len(content_before) >= 1 and content_before[-1] == " ":
             logger.debug(
                 "content_before ends with space; replacing with non-breaking space"
@@ -76,19 +127,18 @@ def link_footnotes(item: Union[Article, Page]) -> None:
             first_footnote = match
 
             # Add the consumed footnote back
-            processed_content.append(f"[ref{note_num}]")
+            processed_content.append(footnote_name)
             break
 
-        if note_num not in footnote_references:
-            footnote_references[note_num] = []
+        if footnote_name not in footnotes:
+            footnotes[footnote_name] = Footnote(len(footnotes) + 1)
 
-        ref_idx = len(footnote_references[note_num])
-        ref_name = REF_SUBNAMES[ref_idx : (ref_idx + 1)]
+        footnote = footnotes[footnote_name]
 
-        footnote_references[note_num].append(ref_name)
+        ref_id = footnote.add_ref()
 
         processed_content.append(
-            f'[<a href="#note-{note_num}" id="ref-{note_num}{ref_name}">{note_num}</a>]'
+            f'[<a href="#{footnote.html_id}" id="{ref_id}">{footnote.num}</a>]'
         )
 
     content_after = item._content[first_footnote.span()[1] :]
@@ -97,8 +147,7 @@ def link_footnotes(item: Union[Article, Page]) -> None:
     content_with_references_processed = "".join(processed_content)
 
     # Process footnotes
-
-    footnote_matches = list(RE_FOOTNOTE.finditer(content_with_references_processed))
+    footnote_matches = list(re_footnote.finditer(content_with_references_processed))
 
     processed_footnotes = []
     footnotes_without_references = []
@@ -107,35 +156,33 @@ def link_footnotes(item: Union[Article, Page]) -> None:
 
     for match in footnote_matches:
         content_before = match.group(1)
-        note_num = match.group(2)
+        footnote_name = match.group(2)
         content_within = match.group(3)
 
         processed_content.append(content_before)
 
-        processed_footnotes.append(note_num)
+        processed_footnotes.append(footnote_name)
 
-        if note_num not in footnote_references:
-            footnotes_without_references.append(note_num)
-            reference_links = ""
-        elif len(footnote_references[note_num]) == 1:
-            reference_links = "".join(
-                [
-                    f'<a href="#ref-{note_num}{ref_name}">^</a>'
-                    for ref_name in footnote_references[note_num]
-                ]
-            )
-            reference_links = f" {reference_links}"
+        if footnote_name not in footnotes:
+            footnotes_without_references.append(footnote_name)
+            processed_content.append(match.group(0))
+            continue
+
+        footnote = footnotes[footnote_name]
+
+        if footnote.ref_count == 1:
+            reference_links = f' <a href="#{footnote.get_ref_html_id(1)}">^</a>'
         else:
             reference_links = "".join(
                 [
-                    f'<a href="#ref-{note_num}{ref_name}">^{ref_name}</a>'
-                    for ref_name in footnote_references[note_num]
+                    f'<a href="#{footnote.get_ref_html_id(i)}">^{footnote.get_ref_name(i)}</a>'
+                    for i in range(1, footnote.ref_count + 1)
                 ]
             )
             reference_links = f" {reference_links}"
 
         processed_content.append(
-            f'<p id="note-{note_num}">[{note_num}] {content_within}{reference_links}</p>'
+            f'<p id="{footnote.html_id}">[{footnote.num}] {content_within}{reference_links}</p>'
         )
 
     content_after = content_with_references_processed[match.span()[1] :]
@@ -143,8 +190,8 @@ def link_footnotes(item: Union[Article, Page]) -> None:
 
     references_without_footnotes = list(
         filter(
-            lambda note_num: note_num not in processed_footnotes,
-            footnote_references.keys(),
+            lambda footnote_name: footnote_name not in processed_footnotes,
+            footnotes.keys(),
         )
     )
 
@@ -205,5 +252,6 @@ def process_pages(generator: PagesGenerator) -> None:
 
 def register() -> None:
     """Register the plug-in with Pelican."""
+    signals.initialized.connect(initalize_plugin)
     signals.article_generator_finalized.connect(process_articles)
     signals.page_generator_finalized.connect(process_pages)
